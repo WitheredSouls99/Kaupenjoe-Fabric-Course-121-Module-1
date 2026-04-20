ab6b6015026035db4a27333380b9c1d0a24cffbc
WitheredSouls_ <lightningdragonstudio@gmail.com>
2026-04-20 07:44:10 -0700

    More setup and added two items.

--- src/main/java/net/withered/mccourse/MCCourseMod.java
+++ src/main/java/net/withered/mccourse/MCCourseMod.java
@@ -2,6 +2,7 @@ package net.withered.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.withered.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class MCCourseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
 	}
 }
