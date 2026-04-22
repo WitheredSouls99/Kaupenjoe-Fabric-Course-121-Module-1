d3b7d765c50c6b51e7c4a49adb805d5a07a38a9e
WitheredSouls_ <lightningdragonstudio@gmail.com>
2026-04-22 07:56:54 -0700

    Added Tooltips and Tags

--- src/main/java/net/withered/mccourse/block/custom/MagicBlock.java
+++ src/main/java/net/withered/mccourse/block/custom/MagicBlock.java
@@ -8,13 +8,18 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.withered.mccourse.item.ModItems;
+import net.withered.mccourse.util.ModTags;
+
+import java.util.List;
 
 public class MagicBlock extends Block {
     public MagicBlock(Settings settings) {
@@ -38,7 +43,12 @@ public class MagicBlock extends Block {
     }
 
     private boolean isValidItem(ItemStack stack) {
-        return stack.getItem() == ModItems.FLUORITE
-                || stack.getItem() == Items.COAL;
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
+        tooltip.add(Text.translatable("tooltip.mccourse.magic_block.tooltip.1"));
+        super.appendTooltip(stack, context, tooltip, options);
     }
 }
--- src/main/java/net/withered/mccourse/item/ModItems.java
+++ src/main/java/net/withered/mccourse/item/ModItems.java
@@ -4,11 +4,16 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.withered.mccourse.MCCourseMod;
 import net.withered.mccourse.item.custom.ChainsawItem;
+
+import java.util.List;
 
 public class ModItems {
     public static final Item FLUORITE = registerItem("fluorite",
@@ -27,7 +32,13 @@ public class ModItems {
     public static final Item STRAWBERRY = registerItem("strawberry",
             new Item(new Item
                     .Settings()
-                    .food(ModFoodComponents.STRAWBERRY)));
+                    .food(ModFoodComponents.STRAWBERRY)) {
+                @Override
+                public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+                    tooltip.add(Text.translatable("tooltip.mccourse.strawberry.tooltip.1"));
+                    super.appendTooltip(stack, context, tooltip, type);
+                }
+            });
 
     public static final Item STARLIGHT_ASHES = registerItem("starlight_ashes",
             new Item(new Item
